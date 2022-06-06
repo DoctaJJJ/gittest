7296cc7c0271bf305e457c399f01fbd3a1913a01
jjj <1354689090@qq.com>
2022-06-06 21:51:16 +0800

    first

---
+++ src/hellow.java
@@ -0,0 +1,15 @@
+
+    public class hellow {
+
+        public String Json(){
+            String driver = "com.mysql.jdbc.Driver";
+
+//这里我的数据库名字是Person，改成你自己的数据库名
+            String url = "jdbc:mysql://localhost:3206/questionnaire";
+            String user = "root";
+            String pwd = "jjsc172412";
+            System.out.println("ssss");
+            return "jjjj";
+        }
+    }
+
