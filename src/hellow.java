c7d34ea1095f3816610ee5eaa6740ee6b35148b8
jjj <1354689090@qq.com>
2022-06-09 15:38:29 +0800

    master合并分支测试

--- src/hellow.java
+++ src/hellow.java
@@ -2,9 +2,7 @@
     public class hellow {
 
         public String Json(){
-            String driver = "com.mysql.jdbc.Driver";
-
-//这里我的数据库名字是Person，改成你自己的数据库名
+            String driver = "com.mysql.jbc.Drive";
             String url = "jdbc:mysql://localhost:3206/questionnaire";
             String user = "root";
             String pwd = "jjsc172412";
